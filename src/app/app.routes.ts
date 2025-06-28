cbf47cc68c6dd0235ab8186465b033b08288dde4
A1eksD <aleks444@gmx.de>
2025-06-28 23:22:13 +0200

    added oder onload component and change theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TableComponent } from './table/table.component';
+import { HomeComponentComponent } from './component/home-component/home-component.component';
 
 export const routes: Routes = [
-    { path: '', component: TableComponent },
+    { path: '', component: HomeComponentComponent },
+    { path: 'table', component: TableComponent }
 ];
